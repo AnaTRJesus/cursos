548882e7f47e1ace334c50beedd120905af83861
AnaTRJesus <anaribeiro183@gmail.com>
2018-04-23 18:05:10 +0100

    Criação do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule} from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { FormComponent } from './form/form.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
+const appRouters: Routes = [
+
+  {path:'cursos', component: CursosComponent},
+  {path:'login', component: LoginComponent},
+  {path:'', component: HomeComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    LoginComponent,
+    FormComponent,
+    CamelCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouters),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
